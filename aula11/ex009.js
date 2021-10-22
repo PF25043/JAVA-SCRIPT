3debbdcf362fb41ab79638954bbc571225c462b2
Pâmela Fernandes <pamelafernandes.fp@gmail.com>
2021-10-22 08:19:45 -0300

    aula11

---
+++ aula11/ex009.js
@@ -0,0 +1,24 @@
+var pais = 'brasil'
+console.log(`Você vive em ${pais}`)
+/*if(pais == 'brasil'){
+    console.log(`Você é Brasileiro!`)
+}
+else{
+    console.log(`Você é estrangeiro`)
+}*/
+
+//OU
+
+if(pais != 'brasil'){
+    console.log(`Você é estrangeiro`)
+}
+else{
+    console.log(`Você é Brasileiro!`)
+}
+    
+
+
+
+
+//vc vive em xxx
+//vc é estrangeiro
